30476f3965ba5c863f6de2f5dbe98f0da408c5af
Mahmud Hasan <60518699+coder-hasan@users.noreply.github.com>
2020-05-17 22:39:24 +0600

    Updated Many things, Scrollbar , Browser Compatibility etc

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,20 @@ $(document).ready(function(){
         centerPadding: '0',
         autoplaySpeed: 5000,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 5,
         slidesToScroll: 1,
         focusOnSelect: true,
         asNavFor: '.slider_for',
         responsive: [
+            {
+            breakpoint: 1751,
+            settings: {
+                centerMode: true,
+                centerPadding: '',
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+            },
             {
             breakpoint: 575,
             settings: {
@@ -37,7 +46,7 @@ $(document).ready(function(){
         autoplay: false,
         dots: false,
         centerMode: true,
-        centerPadding: '15%',
+        centerPadding: '22%',
         autoplaySpeed: 5000,
         speed: 500,
         slidesToShow: 1,
@@ -46,6 +55,15 @@ $(document).ready(function(){
         nextArrow: false,
         asNavFor: '.slider_nav',
         responsive: [
+            {
+            breakpoint: 1751,
+            settings: {
+                centerMode: true,
+                centerPadding: '15%',
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+            },
             {
             breakpoint: 575,
             settings: {
@@ -65,7 +83,11 @@ var $slider = $('.slider_nav');
     var currentSlide;
     var slidesCount;
     var sliderCounter = document.createElement('div');
+    var progressBar = document.createElement('div');
+    // var progressBarLabel = document.createElement('span');
     sliderCounter.classList.add('slider__counter');
+    progressBar.classList.add('slide_nav_progress');
+    // progressBarLabel.classList.add('slider__label');
     
     var updateSliderCounter = function(slick, currentIndex) {
         currentSlide = slick.slickCurrentSlide() + 1;
@@ -75,12 +97,23 @@ var $slider = $('.slider_nav');
 
     $slider.on('init', function(event, slick) {
         $slider.append(sliderCounter);
+        $slider.append(progressBar);
         updateSliderCounter(slick);
+        // progressBar.append(progressBarLabel);
     });
 
     $slider.on('afterChange', function(event, slick, currentSlide) {
         updateSliderCounter(slick, currentSlide);
     });
+    $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {   
+        var calc = ( (nextSlide) / (slick.slideCount-1) ) * 100;
+        
+        progressBar.style.backgroundSize = calc + "% 100%";
+        progressBar.setAttribute("aria-valuenow", calc);
+        
+        // progressBarLabel.innerText =  calc + '% completed' ;
+      });
+    
     }
 
 if ($('.selectpickers').length > 0) {
